091ed948c361b655e0898ac9a8129f46716a3e3d
Steve <stephenvincentibanez@gmail.com>
2020-11-16 17:46:22 -0800

    commit tree 246a7d2579608e1e1eda0c9ffaf8ddc1acb2096b

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+//   function moveDodgerRight() {
+//     let rightNumbers = dodger.style.right.replace("px", "");
+//     let left = parseInt(rightNumbers, 10);
+  
+//     if (left > 180) {
+//       dodger.style.right = `${left + 1}px`;
+//     }
+//   }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
